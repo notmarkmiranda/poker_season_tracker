ad0e8fc9e703fd9b306e8360c9e5416e8a385d8e
Mark Miranda <notmarkmiranda@gmail.com>
2016-10-25 12:28:08 -0600

    update semi-colons

--- app/assets/javascripts/game_row.js
+++ app/assets/javascripts/game_row.js
@@ -1,5 +1,5 @@
 function gameRow() {
   $("div[data-link]").on('click', function() {
     window.location = $(this).attr('data-link');
-  })
-}
+  });
+};
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-  gameRow()
-  participantRow()
-})
+  gameRow();
+  participantRow();
+});
