fb6a58743ec24197e9c5e6006685e66305655b05
Sal Espinosa <sespinos@gmail.com>
2016-11-19 16:54:09 -0700

    Create and refactor games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,6 +2,13 @@ class Api::V1::GamesController < Api::ApiController
   respond_to :json
 
   def update
-    # require 'pry'; binding.pry
+    game    = Game.find(params[:id])
+    game.completed = true
+    # game.calculate_scores
+    game.save
+
+    players = params[:_json]
+    PlayerCreator.new(players, game)
+    respond_with game
   end
 end
