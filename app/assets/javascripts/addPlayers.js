29606980481788ef5a72cf94d0d79974d4fc8aa0
Tyler Merry <tymerry@gmail.com>
2016-11-19 12:41:47 -0700

    add players functionality ruoughed in

--- app/assets/javascripts/addPlayers.js
+++ app/assets/javascripts/addPlayers.js
@@ -1,28 +1,63 @@
-// $(document).ready(function() {
-//   var playerArray = []
-//   getPlayers(playerArray)
-//   debugger
-//   var count = $('#player_count')
-//   count.change(function() {
-//     addPlayerFields(count.val())
-//   })
-// })
-//
-// function addPlayerFields(number) {
-//   for (i = 0; i < number; i++) {
-//     $( ".list-group" ).append(
-//       `<div class='list-group-item list-group-item-action' id="player-${i}">
-//           <input type='text' class='form-control'
-//       </div>`
-//     )
-//   }
-// }
-//
-// function getPlayers (playerArray) {
-//   $.getJSON('/api/v1/participants').then(function (participants) {
-//   })
-// }
-//
-// function thing () {
-//   return ["yes", "no"]
-// }
+$(document).ready(function() {
+  var playerArray = []
+  playerArray = $.getJSON('/api/v1/participants', function (participants) {
+		for (var i = 0; i < participants.length; i++) {
+			participants[i].name = participants[i].first_name +" " + participants[i].last_name
+		}
+		var $input = $('.typeahead')
+		$input.typeahead({ source:participants })
+		$input.change(function() {
+			  var current = $input.typeahead("getActive");
+		    if (current) {
+		        // Some item from your model is active!
+		        if (current.name == $input.val()) {
+								console.log(current)
+								$input.val("")
+								addPlayer(current, "#playersOnly")
+									//
+		            // This means the exact match is found. Use toLowerCase() if you want case insensitive match.
+		        } else {
+		            // This means it is only a partial match, you can either add a new item
+		            // or take the active if you don't want new items
+		        }
+		    } else {
+		        // Nothing is active so it is a new value (or maybe empty value)
+		    }
+		});
+  })
+
+	$("#eliminatedPlayers")
+
+
+	$("#newPlayerButton").click(function(){
+		var name = $('.typeahead').val()
+		if(name){
+			addPlayer({name: name , id: 0}, "#playersOnly");
+			$('.typeahead').val("");
+		}
+	})
+
+})
+
+function addPlayer(player, location) {
+	var options = ""
+
+	if(location === "#playersOnly"){
+		options = '<input type="button" name="name" value="eliminated" class="elim" data-name="' + player.name +'">';
+	}else if(location === "#eliminatedPlayers"){
+		$('input[data-name="'+ player.name +'"]').parent().remove()
+		options= ""
+	}
+
+  $(location).prepend('<div class="player" data-id="'+ player.id + '" data-name="' + player.name +'">' + player.name + options +'</div>')
+
+	if(location === "#playersOnly"){
+		$(".player").on('click','input[data-name="'+ player.name +'"]', function(){
+			addPlayer(player,"#eliminatedPlayers")
+		});
+	}
+}
+
+function thing () {
+  return ["yes", "no"]
+}
