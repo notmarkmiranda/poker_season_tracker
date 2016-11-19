7bfa90d7a0897804064644d0a814626111edcc4f
Sal Espinosa <sespinos@gmail.com>
2016-11-19 12:08:17 -0700

    Add post route for players api

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :participants, only: [:index, :show]
-      resources :players, only: [:index, :show]
+      resources :players, only: [:index, :show, :create]
     end
   end
 
