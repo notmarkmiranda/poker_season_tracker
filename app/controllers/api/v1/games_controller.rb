4c6f4f5a56d7a2e486aad242a50a924afe78e52a
Sal Espinosa <sespinos@gmail.com>
2016-11-22 04:18:43 -0700

    Update JS to redirect after game submit

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::GamesController < Api::ApiController
   respond_to :json
 
   def update
-    game    = Game.find(params[:id])
-    game.completed = true
-    # game.calculate_scores
-    game.save
+    @game    = Game.find(params[:id])
+    @game.completed = true
+    # @game.calculate_scores
+    @game.save
 
     players = params[:_json]
-    PlayerCreator.new(players, game)
-    respond_with game
+    PlayerCreator.new(players, @game)
+    render json: @game.to_json
   end
 end
