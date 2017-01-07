986f3b19bcc4d93c0d4b794a4a3cc882ca1bcf0c
Mark Miranda <notmarkmiranda@gmail.com>
2017-01-06 21:20:01 -0700

    closes #39

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -2,11 +2,39 @@ class Api::V1::ParticipantsController < Api::ApiController
   respond_to :json
 
   def index
-    @participants = Participant.all.sort_by { |p| p.global_score }.reverse
+    render partial: '/api/v1/participants/participants', locals: { participants: Participant.all.sort_by { |p| p.global_score }.reverse }
+    # @participants = Participant.all.sort_by { |p| p.global_score }.reverse
   end
 
   def show
-    @participant = Participant.find(params[:id])
+    participant = Participant.find(params[:id])
+    render partial: '/api/v1/participants/participant', locals: { participant: participant }
+  end
+
+  def create
+    participant = Participant.new(participant_params)
+    render partial: '/api/v1/participants/participant', locals: { participant: participant } if participant.save
+  end
+
+  def update
+    participant = Participant.find(params[:id])
+    render partial: '/api/v1/participants/participant', locals: { participant: participant } if participant.update(participant_params)
+  end
+
+  def destroy
+    participant = Participant.find(params[:id])
+    if participant.delete
+      render partial: '/api/v1/participants/participants', locals: { participants: Participant.all.sort_by { |p| p.global_score }.reverse }
+    else
+    end
+  end
+
+
+
+  private
+
+  def participant_params
+    params.require(:participant).permit(:first_name, :last_name)
   end
 
 end
--- app/views/api/v1/participants/show.json.jbuilder
+++
@@ -1,8 +0,0 @@
-json.name             @participant.display_name
-json.games            @participant.games.count
-json.percent_attended @participant.overall_attendance
-json.score            @participant.global_score
-json.won              @participant.global_win_count
-json.percent_won      @participant.overall_percentage([1])
-json.place            @participant.global_game_top_three_count
-json.percent_placed   @participant.overall_percentage([1,2,3])
